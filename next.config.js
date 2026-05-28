1035a12456d2fb03a3392ebc0db83daf0e8f5905
David M. Graham <onlylivingheartdonor.com>
2026-05-28 11:41:33 -0400

    Sync: final cleanup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  trailingSlash: false,
+  
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'investmentgainstaxcalc.com' }],
+        destination: 'https://www.investmentgainstaxcalc.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
